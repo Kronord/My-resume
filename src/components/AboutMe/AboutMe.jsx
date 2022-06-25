e3c32479f788916b0bc7e513e5a5a1a28de6251d
Dmitrii Shapovalov <87037406+Kronord@users.noreply.github.com>
2022-06-25 23:46:30 +0300

    Some fix

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -12,13 +12,15 @@ export default function AboutMe() {
       <Name>
         Dmitrii <Surname>Shapovalov</Surname>
       </Name>
-      <Spec>Junior React developer</Spec>
+      <Spec>Junior Full Stack developer</Spec>
       <Text>
-        I am a Junior React developer. In projects i used HTML, CSS, JS, React
-        and familiar with technologies such as Next.js, Webpack, Typescript. I
-        prefer to work with data and operations on it, but I also like to
+        I am a Junior Full Stack developer. In projects i used HTML, CSS, JS,
+        React and familiar with technologies such as Next.js, Webpack,
+        Typescript. At the moment, I am finishing studying Node.js and
+        technologies related to it such as: Express, MongoDB, Mongoose, Docker,
+        etc. I prefer to work with data and operations on it, but I also like to
         visualize content and translate ideas into a working web application. I
-        am looking for an opportunity to realize myself as a React / JS
+        am looking for an opportunity to realize myself as a React / Full Stack
         developer, and help companies automate and improve their business.
       </Text>
     </Wrapper>
