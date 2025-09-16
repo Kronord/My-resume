840fd3a32083be7595bd46211dd1489a84c3b8c0
Dmitrii Shapovalov <fedishyn2@gmail.com>
2025-09-16 16:57:52 +0300

    Some new info

--- src/components/Additional Education/AdditionalEducation.jsx
+++ src/components/Additional Education/AdditionalEducation.jsx
@@ -25,6 +25,14 @@ export function AdditionalEducation() {
           </Date>
           <Text>Full Stack Developer</Text>
         </div>
+        <div>
+          <University>SoftServe Academy(Kyiv)</University>
+          <Date>
+            <StyleElement></StyleElement>
+            04/2025 - 06/2025
+          </Date>
+          <Text>Practical React course</Text>
+        </div>
       </Wrapper>
     </>
   );
@@ -45,6 +53,14 @@ export function AdditionalEducationUA() {
             2021 - 2022
           </Date>
           <Text>Full Stack Developer</Text>
+        </div>
+        <div>
+          <University>SoftServe Академія(Київ)</University>
+          <Date>
+            <StyleElement></StyleElement>
+            04/2025 - 06/2025
+          </Date>
+          <Text>Practical React course</Text>
         </div>
       </Wrapper>
     </>
